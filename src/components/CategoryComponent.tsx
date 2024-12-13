439f562b7bd8cf3bbf76f8d0f0e9fae149d718a6
Fauzi Ferdiansyah <fauziferdiansyahindo@gmail.com>
2024-12-13 12:28:34 +0700

    feat: add component category and configure api

--- src/components/CategoryComponent.tsx
+++ src/components/CategoryComponent.tsx
@@ -1,12 +1,67 @@
-//buat class functional component
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import KeenSlider, { KeenSliderInstance } from "keen-slider";
+import "keen-slider/keen-slider.min.css";
+import ItemCatagoryComponent from "./ItemCatagoryComponent";
+import { AppDispatch, RootState } from "../redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategory } from "../redux/slices/CategorySlice";
 
 const CategoryComponent: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const { category } = useSelector((state: RootState) => state.category);
+  const sliderRef = useRef<HTMLDivElement>(null);
+  let slider: KeenSliderInstance | null = null;
+
+  useEffect(() => {
+    dispatch(getCategory());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      slider = new KeenSlider(sliderRef.current, {
+        loop: true,
+        slides: {
+          origin: "center",
+          perView: 4,
+          spacing: 10,
+        },
+      });
+    }
+
+    return () => {
+      if (slider) slider.destroy();
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Category Component</h1>
-      <p>This is the Category Component content.</p>
-    </div>
+    <section>
+      <div className="mx-auto max-w-max px-4 py-12 sm:px-6 lg:me-0 lg:py-16 lg:pe-0 lg:ps-8 xl:py-24">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:items-center lg:gap-16">
+          {/* Bagian teks */}
+          <div className="max-w-lg text-start ltr:sm:text-left rtl:sm:text-right">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              New In <br /> Store Now
+            </h2>
+            <p className="mt-4 text-gray-700">
+              Get the latest items immediately with promo prices
+            </p>
+            <a href="">Check All</a>
+          </div>
+
+          {/* Bagian slider */}
+          <div className="-mx-6 lg:col-span-2 lg:mx-0">
+            <div ref={sliderRef} className="keen-slider">
+              {/* Slide 1 */}
+              {category.map((item, index) => (
+                <div key={index} className="keen-slider__slide">
+                  <ItemCatagoryComponent category={item} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/ItemCatagoryComponent.tsx
+++ src/components/ItemCatagoryComponent.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 
-const ItemCatagoryComponent: React.FC = () => {
+interface ItemCatagoryComponentProps {
+  category: {
+    title: string;
+    image: string;
+  };
+}
+
+const ItemCatagoryComponent: React.FC<ItemCatagoryComponentProps> = ({
+  category,
+}) => {
   return (
-    <div>
-      <h1>Item Catagory Component</h1>
-      <p>This is the Item Catagory Component content.</p>
-    </div>
+    <blockquote className="flex h-full flex-col justify-between bg-white shadow-sm">
+      <div>
+        <a href="#" className="group relative block bg-black">
+          <img
+            alt={category.title}
+            src={category.image}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+          />
+
+          <div className="relative flex flex-col justify-center h-full p-32 sm:p-6 lg:p-8">
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+              <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"></div>
+            </div>
+            <p className="text-md text-center uppercase tracking-widest text-white font-bold">
+              {category.title}
+            </p>
+          </div>
+        </a>
+      </div>
+    </blockquote>
   );
 };
+
 export default ItemCatagoryComponent;
